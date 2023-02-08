8081a8f41823f314c23200e317b17f7d62f4b24e
Artem <Artem@git.local>
2023-02-08 09:19:25 +0300

    added translation files

---
+++ AnalyzePcapUdp/translate_ru.ts
@@ -0,0 +1,245 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="ru_RU">
+<context>
+    <name>ConditionWidget</name>
+    <message>
+        <location filename="nodecondition.cpp" line="21"/>
+        <source>None</source>
+        <translation>Нет</translation>
+    </message>
+    <message>
+        <location filename="nodecondition.cpp" line="22"/>
+        <source>Equal</source>
+        <translation>Равно</translation>
+    </message>
+    <message>
+        <location filename="nodecondition.cpp" line="23"/>
+        <source>Less</source>
+        <translation>Меньше</translation>
+    </message>
+    <message>
+        <location filename="nodecondition.cpp" line="24"/>
+        <source>Greater</source>
+        <translation>Больше</translation>
+    </message>
+    <message>
+        <location filename="nodecondition.cpp" line="26"/>
+        <location filename="nodecondition.cpp" line="32"/>
+        <source>Length</source>
+        <translation>Длина</translation>
+    </message>
+    <message>
+        <location filename="nodecondition.cpp" line="30"/>
+        <source>Condition</source>
+        <translation>Условие</translation>
+    </message>
+    <message>
+        <location filename="nodecondition.cpp" line="31"/>
+        <source>Type</source>
+        <translation>Тип</translation>
+    </message>
+</context>
+<context>
+    <name>CutterWidget</name>
+    <message>
+        <location filename="nodecutter.cpp" line="13"/>
+        <source>Offset</source>
+        <translation>Смещение</translation>
+    </message>
+    <message>
+        <location filename="nodecutter.cpp" line="18"/>
+        <source>Length</source>
+        <translation>Длина</translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>AnalyzePcapUdp</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="40"/>
+        <source>File</source>
+        <translation>Файл</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="45"/>
+        <source>View</source>
+        <translation>Вид</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="58"/>
+        <source>Packet info</source>
+        <translation>Статистика</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="73"/>
+        <source>Select Info Node</source>
+        <translation>Выбрать ноду статистики</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="103"/>
+        <source>Clear</source>
+        <translation>Очистить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="152"/>
+        <source>Scale Reset</source>
+        <translation>Сбросить масштаб</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="157"/>
+        <source>Scale +</source>
+        <translation>Увеличить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="162"/>
+        <source>Scale -</source>
+        <translation>Уменьшить</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="167"/>
+        <source>Move To Center</source>
+        <translation>Центрировать</translation>
+    </message>
+</context>
+<context>
+    <name>NodeCondition</name>
+    <message>
+        <location filename="nodecondition.cpp" line="166"/>
+        <location filename="nodecondition.cpp" line="171"/>
+        <source>Condition Filter</source>
+        <translation>Условие</translation>
+    </message>
+</context>
+<context>
+    <name>NodeCutter</name>
+    <message>
+        <location filename="nodecutter.cpp" line="37"/>
+        <location filename="nodecutter.cpp" line="42"/>
+        <source>Cutter</source>
+        <translation>Вырезать часть</translation>
+    </message>
+</context>
+<context>
+    <name>NodeSource</name>
+    <message>
+        <location filename="nodesource.cpp" line="76"/>
+        <source>Open file</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="nodesource.cpp" line="77"/>
+        <source>Name
+Size</source>
+        <translation>Имя\nРазмер</translation>
+    </message>
+    <message>
+        <location filename="nodesource.cpp" line="86"/>
+        <source>Timeout (ms)</source>
+        <translation>Таймаут(мс)</translation>
+    </message>
+    <message>
+        <location filename="nodesource.cpp" line="115"/>
+        <source>Open File</source>
+        <translation>Открыть файл</translation>
+    </message>
+    <message>
+        <location filename="nodesource.cpp" line="167"/>
+        <source>Name:	&quot;%1&quot;
+</source>
+        <translation>Имя файла:	&quot;%1&quot;\n</translation>
+    </message>
+    <message>
+        <location filename="nodesource.cpp" line="169"/>
+        <source>Size:	%2 KB</source>
+        <translation>Размер:	%2 КБ</translation>
+    </message>
+    <message>
+        <location filename="nodesource.cpp" line="171"/>
+        <source>Size:	%2 MB</source>
+        <translation>Размер:	%2 МБ</translation>
+    </message>
+</context>
+<context>
+    <name>QObject</name>
+    <message>
+        <location filename="nodeudpreceiver.cpp" line="136"/>
+        <source>UDP Receiver</source>
+        <translation>Сокет приема UDP</translation>
+    </message>
+    <message>
+        <location filename="nodefilter.cpp" line="20"/>
+        <source>Address Destination Filter</source>
+        <translation>Фильтр по адресу назначения</translation>
+    </message>
+    <message>
+        <location filename="nodefilter.cpp" line="95"/>
+        <source>Address Source Filter</source>
+        <translation>Фильтр по адресу отправления</translation>
+    </message>
+    <message>
+        <location filename="nodeudpsender.cpp" line="16"/>
+        <source>UDP Sender</source>
+        <translation>Трансмиттер UDP</translation>
+    </message>
+    <message>
+        <location filename="nodeinfopackets.cpp" line="50"/>
+        <source>Info</source>
+        <translation>Статистика</translation>
+    </message>
+    <message>
+        <location filename="registrydatamodel.cpp" line="40"/>
+        <location filename="registrydatamodel.cpp" line="45"/>
+        <source>1. Input</source>
+        <translation>1. Входные ноды</translation>
+    </message>
+    <message>
+        <location filename="registrydatamodel.cpp" line="41"/>
+        <location filename="registrydatamodel.cpp" line="44"/>
+        <source>2. Output</source>
+        <translation>2. Выходные ноды</translation>
+    </message>
+    <message>
+        <location filename="registrydatamodel.cpp" line="42"/>
+        <location filename="registrydatamodel.cpp" line="43"/>
+        <location filename="registrydatamodel.cpp" line="46"/>
+        <location filename="registrydatamodel.cpp" line="47"/>
+        <source>3. Filters</source>
+        <translation>3. Фильтры</translation>
+    </message>
+    <message>
+        <location filename="nodesource.cpp" line="36"/>
+        <source>Source File</source>
+        <translation>Файл дампа (pcap, pcapng)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="96"/>
+        <source>num</source>
+        <translation>#</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="97"/>
+        <source>timestamp</source>
+        <translation>время</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="98"/>
+        <source>id</source>
+        <translation>ид</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="99"/>
+        <source>size</source>
+        <translation>размер (б)</translation>
+    </message>
+    <message>
+        <location filename="mainwindow.cpp" line="100"/>
+        <source>data</source>
+        <translation>данные о пакете</translation>
+    </message>
+</context>
+</TS>
